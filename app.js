940ef597daa3798d03d77723c66b4b439848b66f
saiprasadr870 <105206375+saiprasadr870@users.noreply.github.com>
2024-10-08 17:47:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const port = 3000;
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'sai.prasad@155',
+    password: 'Mrscool@155',
     database: 'social_app'
 });
 
